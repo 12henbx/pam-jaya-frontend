2b25a2ab5e9ebf5a5be9048e29e64f00cdf87393
12henbx <henoch_manampiring@yahoo.com>
2022-07-23 12:41:10 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+import "./assets/img/favico.png";
+import "./assets/img/logo-pam.png";
+import "./assets/img/logo-pam-2.png";
+import "./assets/img/profile-ico.png";
+import "./assets/svg/space_dashboard_black_24dp.svg";
+import "./assets/svg/history_black_24dp.svg";
+import "./assets/svg/settings_black_24dp.svg";
+import "./assets/svg/logout_black_24dp.svg";
+import "./assets/svg/notifications_black_24dp.svg";
+import "./assets/svg/download_black_24dp.svg";
+
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+};
